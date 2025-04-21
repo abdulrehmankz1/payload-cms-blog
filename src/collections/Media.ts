1e42d1cb54c22ab83e3846787ac49b3115381cf5
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2025-04-21 17:33:26 +0500

    Added upload configuration to Media collection

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,7 +1,26 @@
-import type { CollectionConfig } from 'payload'
+import { CollectionConfig } from 'payload'
 
 export const Media: CollectionConfig = {
   slug: 'media',
+  upload: {
+    staticDir: 'media',
+    mimeTypes: ['image/*'],
+    imageSizes: [
+      {
+        name: 'thumbnail',
+        width: 400,
+        height: 300,
+        position: 'centre',
+      },
+      {
+        name: 'full',
+        width: 400,
+        height: 300,
+        position: 'centre',
+      },
+    ],
+    adminThumbnail: 'thumbnail',
+  },
   access: {
     read: () => true,
   },
@@ -11,6 +30,11 @@ export const Media: CollectionConfig = {
       type: 'text',
       required: true,
     },
+    // {
+    //   name: 'post', // Create a relation to the 'posts' collection
+    //   type: 'relationship',
+    //   relationTo: 'posts', // Link to the 'posts' collection
+    //   required: true, // Make the relation mandatory
+    // },
   ],
-  upload: true,
 }
