73a9309210dcfd0296cba4d39b21d290c17e3cb2
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2025-04-24 14:24:00 +0500

    Refactor HomePage component: update layout, styling, and links.

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,7 +2,6 @@ import { headers as getHeaders } from 'next/headers.js'
 import Image from 'next/image'
 import { getPayload } from 'payload'
 import React from 'react'
-import { fileURLToPath } from 'url'
 
 import config from '@/payload.config'
 import './styles.css'
@@ -14,12 +13,10 @@ export default async function HomePage() {
   const payload = await getPayload({ config: payloadConfig })
   const { user } = await payload.auth({ headers })
 
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
-
   return (
-    <div className="home bg-yellow-300">
-      <div className="content">
-        <picture>
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="flex flex-col items-center text-center px-4">
+        <picture className="mb-5">
           <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
           <Image
             alt="Payload Logo"
@@ -28,18 +25,30 @@ export default async function HomePage() {
             width={65}
           />
         </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
+        {!user && <h1 className="text-4xl font-bold">Welcome to your new project.</h1>}
+        {user && (
+          <h1 className="text-5xl font-bold text-center leading-snug">
+            Welcome back,
+            <br />
+            {user.email}
+          </h1>
+        )}
+
+        <div className="flex items-center gap-3 mt-5">
           <Link
-            className="admin"
+            className="bg-white text-black border border-black px-3 py-1 rounded hover:bg-gray-100 transition"
             href={payloadConfig.routes.admin}
             rel="noopener noreferrer"
             target="_blank"
           >
             Go to admin panel
           </Link>
-          <Link href="/posts">View All Posts</Link>
+          <Link
+            className="bg-black text-white px-3 py-1 rounded hover:underline transition"
+            href="/posts"
+          >
+            View All Posts
+          </Link>
         </div>
       </div>
     </div>
